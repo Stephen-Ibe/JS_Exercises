192f1077be23367dd015179e2441f3866d46932c
KalsonSaint <saintkalson@gmail.com>
2019-09-16 17:22:25 +0100

    JS EXERCISE

---
+++ Sum of Two Numbers.js
@@ -0,0 +1,10 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+// arrays for testing
+// (addition(2, 3), 5)
+// (addition(-3, -6), -9)
+// (addition(7, 3), 10)
+
+function addition(a, b) {
+	return a+b;
+}
---
+++ Total Number of Digits the Given Number Has.js
@@ -0,0 +1,15 @@
+// Return the First Element in an Array
+    // Test Conditions
+// (getFirstValue([1, 2, 3]), 1)
+// (getFirstValue([80, 5, 100]), 80)
+// (getFirstValue([-500, 0, 50]), -500)
+// (getFirstValue([5, 2, 3]), 5)
+// (getFirstValue([75675, 5, 100]), 75675)
+// (getFirstValue([-52320, 0, 50]), -52320)
+
+// Create a function that takes an array and returns the first element.
+function getFirstValue(arr) {
+	for(let i = 0; i<1;i++){
+		return arr[i];
+	}
+}
